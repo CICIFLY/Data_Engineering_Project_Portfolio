f2cf6d686296cc59a0a08fa53fec0677da232782
CICIFLY <xizhou24@yahoo.com>
2019-04-20 14:06:02 -0600

    in sql_queries.py: changed the on conflict do update expression in user_table_insert statement

--- Data_Modeling_with_Postgres/sql_queries.py
+++ Data_Modeling_with_Postgres/sql_queries.py
@@ -40,7 +40,7 @@ time_table_create = (""" CREATE TABLE IF NOT EXISTS time ( start_time timestamp 
 # songplay_id will increase automatically, it should not be treated as a separate column 
 songplay_table_insert = (""" INSERT INTO songplays ( start_time, user_id, level, song_id, artist_id, session_id, location , user_agent) VALUES ( %s, %s, %s, %s, %s, %s, %s, %s) """)   
 
-user_table_insert = (""" INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO UPDATE SET level='paid' """)
+user_table_insert = (""" INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO UPDATE SET level= EXCLUDED.level """)
 
 song_table_insert = (""" INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (song_id) DO NOTHING """)
 
